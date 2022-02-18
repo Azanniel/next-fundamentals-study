aaf1bb84b57e28a598c992ea42e1a8d43a98e61e
Azanniel <leo.azannielttt@gmail.com>
2022-02-17 22:59:36 -0400

    feat: client side rendering - repos of my github profile

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,27 @@
+import { useEffect, useState } from "react"
+
 export default function Home() {
+  const [repositories, setRepositories] = useState<string[]>([])
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/azanniel/repos')
+      .then(response => response.json())
+      .then(data => {
+        const repositoryNames = data.map(item => item.name)
+
+        setRepositories(repositoryNames)
+      })
+  }, [])
+
   return (
-    <h1>Hello world</h1>
+    <>
+      <h2>Repositórios</h2>
+
+      <ul>
+        {repositories.map(repo => (
+          <li key={repo}>{repo}</li>
+        ))}
+      </ul>
+    </>
   )
 }
